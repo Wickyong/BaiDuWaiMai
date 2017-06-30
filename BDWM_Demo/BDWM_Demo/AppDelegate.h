4d7d2421439b8b1ec2b455a4e4b36c5cd65bf115
WickYong <wy46817@gmail.com>
2017-06-30 20:54:35 +0800

    案例准备

---
+++ BDWM_Demo/BDWM_Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BDWM_Demo
+//
+//  Created by WangYong on 2017/6/30.
+//  Copyright © 2017年 WangYong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BDWM_Demo/BDWM_Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BDWM_Demo
+//
+//  Created by WangYong on 2017/6/30.
+//  Copyright © 2017年 WangYong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
